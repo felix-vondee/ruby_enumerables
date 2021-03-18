dd3808ea2912e47f854b24fcdfaee768dc06f6f8
felix_vondee <felixvondee8@gmail.com>
2021-03-18 16:59:00 +0200

    Fix errors

--- script.rb
+++ script.rb
@@ -33,47 +33,4 @@ module Enumerable
     end
     final
   end
-
-  def my_all?(block = nil)
-    my_each do |re|
-      if block_given?
-        return false unless yield re
-      elsif block.instance_of?(Regexp)
-        return false unless block =~ re
-      elsif block.instance_of?(Class)
-        return false unless [re.class, re.class.superclass].include?(block)
-      elsif !block.nil?
-        return false unless re == block
-      else
-        return false unless re
-      end
-    end
-    true
-  end
-
-  def my_any?(block = nil)
-    arr = to_a
-    return false if (arr - [nil, false]) == []
-
-    my_each do |re|
-      if block_given?
-        return true if yield re
-      elsif block.instance_of?(Regexp)
-        return true if block =~ re
-      elsif block.instance_of?(Class)
-        return true if [re.class, re.class.superclass].include?(block)
-      elsif !block.nil?
-        my_boolean = false
-        arr.each do |i|
-          my_boolean = true if i == block
-        end
-        return my_boolean
-      else
-        return true unless re
-      end
-    end
-    return true if block.nil? && !block_given?
-
-    false
-  end
 end
